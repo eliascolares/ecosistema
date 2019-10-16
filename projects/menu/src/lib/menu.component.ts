56f102ed0a5302fc09c7f4457e9f66c729873279
Elias Colares <elias@dcide.com.br>
2019-10-16 14:07:25 -0300

    Estrutura inicial

---
+++ projects/menu/src/lib/menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-menu',
+  template: `
+    <p>
+      menu works!
+    </p>
+  `,
+  styles: []
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
