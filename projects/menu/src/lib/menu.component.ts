4cdbb8ba3bfca1b87676d0526382c73db10f3d87
Elias Colares <elias@dcide.com.br>
2019-10-16 15:15:49 -0300

    Estrutura inicial do menu

--- projects/menu/src/lib/menu.component.ts
+++ projects/menu/src/lib/menu.component.ts
@@ -1,19 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-menu',
-  template: `
-    <p>
-      menu works!
-    </p>
-  `,
-  styles: []
+  templateUrl: 'menu.component.html',
+  styleUrls: ['menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+
+  @Input() menuItems: [];
+  @Input() logoUrl: string;
+  @Input() name: string;
+
+  config = {
+    paddingAtStart: true,
+    classname: 'my-custom-class',
+    listBackgroundColor: 'transparent',
+    fontColor: '#ffffff',
+    backgroundColor: '#3d3d3d',
+    selectedListFontColor: '#40b000',
+    interfaceWithRoute: true
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onLogout(): void {
+    console.log('Logout');
+  }
+
+  selectItem(sidenav: any): void {
+    if (sidenav._opened) {
+      sidenav.toggle();
+    }
   }
 
 }
--- projects/menu/src/lib/menu.module.ts
+++ projects/menu/src/lib/menu.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { MenuComponent } from './menu.component';
-
-
+import { MatIconModule, MatMenuModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+import { NgMaterialMultilevelMenuModule } from 'ng-material-multilevel-menu';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [MenuComponent],
   imports: [
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    NgMaterialMultilevelMenuModule,
+    RouterModule
   ],
   exports: [MenuComponent]
 })
